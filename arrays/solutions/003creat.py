a7ef100cad5bae5e84f6d79fec72225623284b1b
mallimuondu <mallimuondu@gmail.com>
2020-10-14 14:36:47 +0300

    addded code

---
+++ arrays/solutions/003creat.py
@@ -0,0 +1,3 @@
+import array as ai
+a = ai.array('b', [1, 2, 3])
+print(len(a))
---
+++ arrays/solutions/004add.py
@@ -0,0 +1,6 @@
+import array as ar
+
+numbers = ar.array('b Output: array('i', [1, 2, 3, 4])', [1, 2, 3])
+
+numbers.append(4)
+print(numbers)    
