0d0d9ecd1e8525ed0853af5c14a75215d23283c2
moundu <munenemuondu@gmail.com>
2020-10-21 11:08:09 +0300

    I have made some changes

--- arrays/Solutions/005elemntAtPositionInArray.py
+++ arrays/Solutions/005elemntAtPositionInArray.py
@@ -1,7 +1,9 @@
 input1 = input("Enter the elements you want:  ")
+if len(input1) == 0:
+    print("pls input more than 1 thing")
+else:
+    print(input1[0])
 
-print(input1[0])
+    array1 = []
 
-array1 = []
-
-array1.append(input1)
+    array1.append(input1)
