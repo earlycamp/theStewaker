c3dfb1a9e9c3f10aee278fff19b52d33571b656d
moundu <munenemuondu@gmail.com>
2020-10-21 10:48:54 +0300

    I have made some changes

--- arrays/Solutions/005elemntAtPositionInArray.py
+++ arrays/Solutions/005elemntAtPositionInArray.py
@@ -1,16 +1,7 @@
 input1 = input("Enter the elements you want:  ")
-input2 = input("Enter the elements you want:  ")
-input3 = input("Enter the elements you want:  ")
-input4 = input("Enter the elements you want:  ")
-input5 = input("Enter the elements you want:  ")
 
+print(input1[0])
 
 array1 = []
 
 array1.append(input1)
-array1.append(input2)
-array1.append(input3)
-array1.append(input4)
-array1.append(input5)
- 
-print(array1[0])
